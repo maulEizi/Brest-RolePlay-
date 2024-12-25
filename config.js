cbe48592f1fa10c7899c7bb541641f69540ff238
maulEizi <Pauldubled@outlook.fr>
2024-12-25 17:54:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
-        funny: '1251209545129132110',  // Buyer
+        buyer: '915321015515643935', // Buyer
+        funny: '915321015515643935',  // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
-        maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre
+        footer: 'Brest RolePlay', // Dans la commande help ou autre il y'a un texte en bas
+        maxServer: '1', // Le nombre de serveur maximum que le bot peut rejoindre
     }
 }
